500ec97f2250fd12479028a9bac4f7cc9177a26f
TopTalentDev <toptal715@gmail.com>
2018-01-18 09:32:02 +0200

    add side collpased panel

---
+++ src/app/components/keywords/accordion.controller.js
@@ -0,0 +1,38 @@
+/**=========================================================
+ * Module: accordion.controller.js
+ * accordion controller
+ =========================================================*/
+
+(function () {
+    'use strict';
+
+    angular
+        .module('app.keywords')
+        .controller('accordionController', accordionController);
+
+    accordionController.$inject = ['$scope', '$timeout', '$resource', '$q', '$location'];
+
+    function accordionController($scope, $timeout, $resource, $q, $location) {
+        /* jshint validthis:true */
+        var vm = this;
+
+        activate();
+
+        //////////////
+
+        function activate () {
+
+            vm.panels = [
+                { id: 0, selected: true, view: 'app/views/partials/keywords-dashboard.html' },
+                { id: 1, selected: false, view: 'app/views/partials/keywords-list.html' },
+            ];
+
+            vm.leaveCollapse = function(id) {                        
+                for(var item in vm.panels) {
+                    vm.panels[item].selected = ( id == item ) ? true : false;
+                } 
+            };
+        }
+    }
+
+})();
