f6b622819f1784e5ed4deda8476bb48c1dcc34f1
TopTalentDev <toptal715@gmail.com>
2018-03-08 13:01:20 +0200

    add tooltips, fix responsive issue

--- src/app/components/keywords/common/keywords.routes.js
+++ src/app/components/keywords/common/keywords.routes.js
@@ -33,81 +33,27 @@
         })
         .state("app.keywords.dashboard", {
             url: "/dashboard",
-            title: "Keywords",
-            templateUrl: helper.componentPath("keywords/common", "keywords"),
-            resolve: helper.resolveFor(
-                "smart-table",
-                "ui.select",
-                "ng-dialog",
-                "ng-bootstrap-select",
-                "highcharts",
-                "highcharts-export"
-            )
+            title: "Keywords"
         })
         .state("app.keywords.dashboard.statistics", {
             url: "/statistics",
-            title: "Keywords",
-            templateUrl: helper.componentPath("keywords/common", "keywords"),
-            resolve: helper.resolveFor(
-                "smart-table",
-                "ui.select",
-                "ng-dialog",
-                "ng-bootstrap-select",
-                "highcharts",
-                "highcharts-export"
-            )
+            title: "Keywords"
         })
         .state("app.keywords.dashboard.engines", {
             url: "/engines",
-            title: "Keywords",
-            templateUrl: helper.componentPath("keywords/common", "keywords"),
-            resolve: helper.resolveFor(
-                "smart-table",
-                "ui.select",
-                "ng-dialog",
-                "ng-bootstrap-select",
-                "highcharts",
-                "highcharts-export"
-            )
+            title: "Keywords"
         })
         .state("app.keywords.dashboard.ranking", {
             url: "/ranking",
-            title: "Keywords",
-            templateUrl: helper.componentPath("keywords/common", "keywords"),
-            resolve: helper.resolveFor(
-                "smart-table",
-                "ui.select",
-                "ng-dialog",
-                "ng-bootstrap-select",
-                "highcharts",
-                "highcharts-export"
-            )
+            title: "Keywords"
         })
         .state("app.keywords.list", {
             url: "/list",
-            title: "Keywords",
-            templateUrl: helper.componentPath("keywords/common", "keywords"),
-            resolve: helper.resolveFor(
-                "smart-table",
-                "ui.select",
-                "ng-dialog",
-                "ng-bootstrap-select",
-                "highcharts",
-                "highcharts-export"
-            )
+            title: "Keywords"
         })
         .state("app.keywords.add-keywords", {
             url: "/add-keywords",
-            title: "Keywords",
-            templateUrl: helper.componentPath("keywords/common", "keywords"),
-            resolve: helper.resolveFor(
-                "smart-table",
-                "ui.select",
-                "ng-dialog",
-                "ng-bootstrap-select",
-                "highcharts",
-                "highcharts-export"
-            )
+            title: "Keywords"
         });
     }
 })(angular);
