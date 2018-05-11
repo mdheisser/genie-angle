c23ff03caaafb027e8a9b07e84cb9ab4d1455286
TopTalentDev <toptal715@gmail.com>
2018-05-11 10:42:00 +0300

    init serverless

---
+++ functions/genrated/handler.js
@@ -0,0 +1,23 @@
+const others = require('./others.js');
+
+module.exports = {
+  getKeywords: others.getKeywords,
+  getKeyword-detail: others.getKeyword-detail,
+  getKeywordActive-default: others.getKeywordActive-default,
+  getKeywordDeactive-default: others.getKeywordDeactive-default,
+  getKeywordActive-forced: others.getKeywordActive-forced,
+  getKeywordDeactive-forced: others.getKeywordDeactive-forced,
+  getKeywordActive-promoted: others.getKeywordActive-promoted,
+  getKeywordDeactive-promoted: others.getKeywordDeactive-promoted,
+  getKeywordActive-monitored: others.getKeywordActive-monitored,
+  getKeywordDeactive-monitored: others.getKeywordDeactive-monitored,
+  getPages: others.getPages,
+  getPageViolation: others.getPageViolation,
+  postAccountLogin: others.postAccountLogin,
+  getUsers: others.getUsers,
+  getUsersUserInfo: others.getUsersUserInfo,
+  getSites: others.getSites,
+  getSitesCheckSite: others.getSitesCheckSite,
+  getSites: others.getSites,
+  postSitesAddNew: others.postSitesAddNew,
+};
