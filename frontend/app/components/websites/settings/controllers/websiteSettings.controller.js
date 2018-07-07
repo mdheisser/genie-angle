9065db580d7883fa794bc6944aa374edd11fc5bf
TopTalentDev <toptal715@gmail.com>
2018-07-07 09:52:37 +0800

    fix website settings page issues

--- frontend/app/components/websites/settings/controllers/websiteSettings.controller.js
+++ frontend/app/components/websites/settings/controllers/websiteSettings.controller.js
@@ -5,11 +5,14 @@
         .module('components.websites')
         .controller('websiteSettingsController', websiteSettingsController)
 
-    websiteSettingsController.$inject = ['$scope', '$timeout', '$state'];
+    websiteSettingsController.$inject = ['$scope', '$timeout', '$state', 'commonService'];
 
-    function websiteSettingsController($scope, $timeout, $state) {
+    function websiteSettingsController($scope, $timeout, $state, commonService) {
         /* jshint validthis:true */
         var vm = this;
+
+        vm.maximumKeywordByPage = '5';
+        vm.appliedDefaultKeywords = true;
 
         activate();
 
@@ -20,31 +23,44 @@
                     name: 'Excellent',
                     value: '4',
                     bg: 'bg-green-300',
-                    significanceSelected: false,
-                    suitabilitySelected: false
+                    significanceSelected: true,
+                    suitabilitySelected: true
                 },
                 {
                     name: 'Good',
                     value: '3',
                     bg: 'bg-green-500',
-                    significanceSelected: false,
-                    suitabilitySelected: false
+                    significanceSelected: true,
+                    suitabilitySelected: true
                 },
                 {
                     name: 'Suitable',
                     value: '2',
                     bg: 'bg-yellow-700',
-                    significanceSelected: false,
-                    suitabilitySelected: false
+                    significanceSelected: true,
+                    suitabilitySelected: true
                 },
                 {
                     name: 'Poor',
                     value: '1',
                     bg: 'bg-red-500',
                     significanceSelected: false,
-                    suitabilitySelected: false
+                    suitabilitySelected: true
                 }
             ];
+            getOwnSites(1);
+        }
+
+        // Get user's own site names.
+        function getOwnSites(userID) {
+            commonService
+                .getUserSites(userID)
+                .then(function (response) {
+                    vm.sites = response.data;
+                    vm.selectedSite = vm.sites[0];
+                    getKeywords(vm.selectedSite.id);
+                    getLanguages(vm.selectedSite.id);
+                });
         }
     }
 
