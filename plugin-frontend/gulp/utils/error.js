61ffaf3ce92d1b4ba58f5808eac7a6c99f380429
TopTalentDev <toptal715@gmail.com>
2018-09-26 21:56:34 +0800

    reorganization of wp plugin

--- plugin-frontend/gulp/utils/error.js
+++
@@ -1,6 +0,0 @@
-const $ = require('gulp-load-plugins')();
-
-module.exports = function (err) {
-	$.util.log($.util.colors.red(err.toString()));
-    this.emit('end');
-}
--- plugin-frontend/gulp/utils/log.js
+++
@@ -1,5 +0,0 @@
-const $ = require('gulp-load-plugins')();
-
-module.exports = function (msg) {
-    $.util.log($.util.colors.blue(msg));
-}
